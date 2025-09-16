c46f415f9784c150a334af3cc60fd36c2009182d
majdeddine oumanni <majdeddineoumanni@gmail.com>
2025-09-16 23:01:34 +0100

    creating the environment

---
+++ src/main/java/com/group/keycloakenvironment/KeycloakEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.group.keycloakenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakEnvironmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/keycloakenvironment/KeycloakEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.keycloakenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
